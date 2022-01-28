37e9a3372c14b25f1372c039f9bcf68d4c987ad1
tmampa <tmampa94@gmail.com>
2022-01-28 09:59:57 +0200

    Add book input

---
+++ src/components/Books.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Books = () => {
+  <div>
+    <h3>Book list</h3>
+    <button>Remove book</button>
+    <div>
+      <input type="text" placeholder="Add a book" />
+    </div>
+  </div>
+}
+
+export default Books;
