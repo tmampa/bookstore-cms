5e97b9b4a598ccedec5e9b75a203c70c170b19df
tmampa <tmampa94@gmail.com>
2022-01-28 22:51:33 +0200

    Add a book component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import Books from './components/Books';
+import Books from './components/Book';
 import Categories from './components/Categories';
 import Navbar from './components/Navbar';
 
