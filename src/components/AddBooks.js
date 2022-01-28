fedbac2bb0dcb9f8516c1b5d25629db0c5aea3c9
tmampa <tmampa94@gmail.com>
2022-01-28 22:10:59 +0200

    Add new book component

---
+++ src/components/AddBooks.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBooks } from '../redux/books/books';
+
+export default function AddBooks() {
+  const books = useSelector((state) => state.books);
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const addNewBook = (e) => {
+    if (title && author ) {
+      e.preventDefault();
+      const data = {
+        id: books.length + 1,
+        title,
+        author,
+      };
+      dispatch(addBooks(data));
+      setTitle('');
+      setAuthor('');
+    }
+  }
+  return (
+    <div>
+      <form>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder='Add new book' />
+        <input value={author} onChange={(e) => setAuthor(e.target.value)} placeholder='Author' />
+      </form>
+    </div>
+  )
+}
