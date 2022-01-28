639b28b9d177fe93ec0c0f84add74f03a62b4882
tmampa <tmampa94@gmail.com>
2022-01-28 22:37:11 +0200

    Add navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => (
+  <ul>
+    <li>
+      <Link to="/">Bookly</Link>
+    </li>
+    <li>
+      <Link to="/categories">Categories</Link>
+    </li>
+  </ul>
+);
+
+export default Navbar;
